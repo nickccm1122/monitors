ab57be1aec1fa829a6ed7491419f05ea28dd7da8
Nick Chan <nickchan@hk01.com>
2017-09-16 18:31:13 +0800

    add pm2 config

---
+++ pm2.config.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const pkg = require('./package')
+
+module.exports = {
+  apps: [
+    {
+      name: pkg.name,
+      script: 'yarn',
+      args: 'start',
+      merge_logs: true,
+      restart_delay: 5000,
+      env: {
+        watch: false,
+        autorestart: true
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: ['monitors'],
+      path: '/srv',
+      ref: 'origin/master',
+      repo: 'git@github.com:nickccm1122/monitors.git',
+      'post-deploy': 'yarn',
+      env: {
+        NODE_ENV: 'production'
+      }
+    }
+  }
+}
