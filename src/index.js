dda380b83ca9032476e6a94438d8369c7204c205
Nick Chan <nickchan@hk01.com>
2017-09-16 19:03:00 +0800

    kill cron on process exit

--- src/index.js
+++ src/index.js
@@ -7,14 +7,18 @@ const config = require(`${rootPath}/src/config`)
 const logger = require(`${rootPath}/src/utils/logger`)
 const { triplehGameCron } = require(`${rootPath}/src/crons`)
 
-process.on('unhandledRejection', error => {
-  logger.error('unhandledRejection', error.stack)
-})
-
 const app = new Koa()
 
 app.listen(config.port, () => {
   logger.info('Server started..')
 
   triplehGameCron.start()
+})
+
+process.on('unhandledRejection', error => {
+  logger.error('unhandledRejection', error.stack)
+})
+
+process.on('SIGTERM', () => {
+  triplehGameCron.stop()
 })
