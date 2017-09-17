ed1af0b441e8a0b50a2ca388336f289c0697c21d
Nick Chan <nickchan@hk01.com>
2017-09-17 17:07:41 +0800

    run job every 3 minutes

--- src/crons/triplehGameCron/index.js
+++ src/crons/triplehGameCron/index.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const moment = require('moment-timezone')
-const R = require('ramda')
 const rootPath = require('pkg-dir').sync(__dirname)
 const schedule = require('node-schedule')
 
@@ -14,15 +13,13 @@ const triplehGameCron = {
   start: () => {
     const job = schedule.scheduleJob({
       end: moment.tz('2017-09-24', config.momentTimeZone),
-      rule: '*/15, * * * * *'
+      rule: '* */3 * * * *'
     }, async () => {
       try {
         const rankings = await triplehGameCrawling()
 
         if (!rankings) {
           await notifier.sendMessage(`RankingPage is not working!!!`)
-        } else {
-          await notifier.sendMessage(JSON.stringify(R.slice(0, 3, rankings), null, 2))
         }
 
         logger.info(`next crawling will be performed at ${job.nextInvocation()}`)
