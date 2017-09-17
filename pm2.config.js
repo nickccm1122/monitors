bd69cfecfea699a13c5e10362e2a2b177e3a802d
Nick Chan <nickchan@hk01.com>
2017-09-17 15:21:59 +0800

    test

--- pm2.config.js
+++ pm2.config.js
@@ -26,7 +26,7 @@ module.exports = {
       path: `/srv/${pkg.name}`,
       ref: 'origin/master',
       repo: 'git@github.com:nickccm1122/monitors.git',
-      'post-deploy': 'yarn',
+      'post-deploy': 'echo $PATH',
       env: {
         NODE_ENV: 'production'
       }
